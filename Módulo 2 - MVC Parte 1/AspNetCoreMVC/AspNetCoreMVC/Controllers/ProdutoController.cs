fe2806a2faee3d79c2cc5323adf74cc16e039456
Paulo Lima <paulo.lima@expertsystem.com.br>
2020-09-16 18:51:19 -0300

    Aula de HttpContext e de IActionResult parametros

--- Módulo 2 - MVC Parte 1/AspNetCoreMVC/AspNetCoreMVC/Controllers/ProdutoController.cs
+++ Módulo 2 - MVC Parte 1/AspNetCoreMVC/AspNetCoreMVC/Controllers/ProdutoController.cs
@@ -3,14 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 
 namespace AspNetCoreMVC.Controllers
 {
     public class ProdutoController : Controller
     {
-        public string Index()
+        public IActionResult Index(int? pagina, string ordem)
         {
-            return "Este é o metódo Action Padrão";
+            //Aula de HttpContext
+            //var https = HttpContext.Request.IsHttps;
+            //var caminho = HttpContext.Request.Path;
+            //var status = HttpContext.Response.StatusCode;
+            //var conexao = HttpContext.Connection.LocalIpAddress;
+            //return https + "\r\n" + caminho + "\r\n" + status + "\r\n" + conexao;
+
+            // Aula de parametros
+            if (!pagina.HasValue) pagina = 1;
+
+            if (String.IsNullOrEmpty(ordem)) ordem = "nome";
+
+            return Ok(String.Format("pagina={0}&ordem={1}", pagina, ordem));
         }
 
         public IActionResult Detalhe()
@@ -22,6 +35,11 @@ namespace AspNetCoreMVC.Controllers
             //return Content("Arquivo PDF", "application/pdf"); /*Testando o retorno em um arquivo PDF*/
             //return RedirectToAction("Index", "Home", new { teste = "Testando a passagem de parametros por redirecionamento" });
             return View();
+        }
+
+        public IActionResult Edit(int id)
+        {
+            return Ok("O valor de Id = " + id);
         }
     }
 }
